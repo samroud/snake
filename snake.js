08b3e3da8551d1892a9ce88317f36d0a090fb2d1
samroud <sam@supero.ch>
2021-10-18 16:08:29 +0200

    Snake move

--- snake.js
+++ snake.js
@@ -1,17 +1,32 @@
-let snakeSize = 10
-let xPos = 0;
-let yPos = 100
-let xSpeed = 2
-let ySpeed = 2
+let snakeSize = 30
+let xPos
+let yPos
+let xSpeed = snakeSize
+let ySpeed = snakeSize
 let direction
 let directionRandom
 let xDirection
 let yDirection
+let snakeWay = []
+let appleXPos
+let appleYPos
+let snakeLength = 1
+let widthCanvas = 1000
+
+function appleMove(){
+  appleXPos = snakeSize * Math.floor(Math.random() * windowWidth/snakeSize + 1) - snakeSize/2;
+  appleYPos = snakeSize * Math.floor(Math.random() * windowHeight/snakeSize + 1) - snakeSize/2;
+}
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    xPos = Math.floor(Math.random() * windowWidth) + snakeSize;
-    yPos = Math.floor(Math.random() * windowHeight) + snakeSize;
+    frameRate(10)
+    xPos = snakeSize * 10//snakeSize * Math.floor(Math.random() * windowWidth/snakeSize);
+    yPos = snakeSize * 10 //snakeSize * Math.floor(Math.random() * windowHeight/snakeSize);
+    appleMove()
+    textSize(60)
+    textAlign(CENTER)
   }
   
   function mouseClicked() {
@@ -20,8 +35,7 @@ function setup() {
     yDirection = mouseY-yPos
     console.log(xDirection+" / "+yDirection)
 
-    //DIRECTION
-
+    //MOVE WITH MOUSE
     //En bas à droite
     if (xDirection > 0 && yDirection > 0){
       if (xDirection < yDirection){
@@ -37,7 +51,6 @@ function setup() {
       }
       console.log("en bas à droite")
     }
-
     //En bas à gauche
     if (xDirection < 0 && yDirection > 0){
       yDirection = -yDirection
@@ -55,7 +68,6 @@ function setup() {
       yDirection = -yDirection
       console.log("en bas à gauche")
     }
-
     //En haut à droite
     if (xDirection > 0 && yDirection < 0){
       xDirection = -xDirection
@@ -73,7 +85,6 @@ function setup() {
       xDirection = -xDirection
       console.log("en haut à droite")
     }
-
      //En haut à gauche
      if (xDirection < 0 && yDirection < 0){
       if (xDirection > yDirection){
@@ -88,29 +99,110 @@ function setup() {
         ySpeed=-ySpeed
       }
       console.log("en haut à gauche")
+      
     }
-    console.log(direction)
+    //console.log(direction)
+
   }
   
 
   function draw() {
     background(255);
+    
+    //GRID
+    stroke(0,0,0,30)
+    let colNum = windowWidth/snakeSize
+    let rowNum = windowHeight/snakeSize
+    for(let row=0;row<rowNum;row++){
+      push()
+      translate(0,row*snakeSize)
+      line(0,0,windowWidth,0)
+      pop()
+    }
+    for(let col=0;col<colNum;col++){
+      push()
+      translate(col*snakeSize,0)
+      line(0,0,0,windowHeight)
+      pop()
+    }
+
     fill(0)
-    rect(xPos, yPos, snakeSize,snakeSize)
+
 
     //SNAKE WALL
-    if (xPos > windowWidth-snakeSize){
-      xPos = 0
+    if (xPos > windowWidth){
+      xPos =0
     } 
     if (xPos < 0){
-      xPos = windowWidth-snakeSize
+      xPos = Math.floor(windowWidth/snakeSize) * snakeSize
     } 
-    if (yPos > windowHeight-snakeSize){
+    if (yPos > windowHeight - snakeSize/4){
       yPos = 0
     } 
-    if (yPos < 0){
-      yPos = windowHeight-snakeSize
+    if (yPos+snakeSize < 0){
+      yPos = Math.floor(windowHeight/snakeSize) * snakeSize
     } 
+
+    //WAY
+    snakeWay.push({
+      x: xPos, 
+      y: yPos, 
+    })
+    //console.log(snakeWay)
+
+    if (frameCount>snakeLength){
+      for (let i=1;i<snakeLength+1;i++){
+        snakeLastPositions = snakeWay[snakeWay.length-i]
+        rect(snakeLastPositions.x, snakeLastPositions.y, snakeSize,snakeSize)
+        //COLLISION???
+        if (i>2 && snakeWay[snakeWay.length-1].x==snakeLastPositions.x && snakeWay[snakeWay.length-1].y==snakeLastPositions.y){
+          console.log("COLLISTION!!!")
+          text('GAME OVER', windowWidth/2, 130)
+          xSpeed = 0
+          ySpeed = 0
+          noLoop()
+        }
+
+      }
+      // MIAM MIAM
+      if (snakeWay[snakeWay.length-1].x==appleXPos- snakeSize/2 && snakeWay[snakeWay.length-1].y==appleYPos- snakeSize/2){
+        snakeLength +=1
+        appleMove()
+        console.log("Miam Miam")
+      }      
+    }
+
+
+    //APPLE
+    fill(255,0,0)
+    ellipse(appleXPos,appleYPos,snakeSize,snakeSize)
+
+     //MOVE WITH KEYBOARD
+     if (keyIsDown(LEFT_ARROW)) {
+      direction = "x"
+      if (xSpeed > 0 ){
+        xSpeed=-xSpeed
+      }
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      direction = "x"
+      if (xSpeed < 0 ){
+        xSpeed=-xSpeed
+      }
+    }
+    if (keyIsDown(UP_ARROW)) {
+      direction = "y"
+      if (ySpeed > 0 ){
+        ySpeed=-ySpeed
+      }
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      direction = "y"
+      if (ySpeed < 0 ){
+        ySpeed=-ySpeed
+      }
+    }
+
 
     //SNAKLE MOVE
     if (direction == "x"){
@@ -118,6 +210,5 @@ function setup() {
     } else{
       yPos +=ySpeed
     }
-    //noLoop();
 
   }
